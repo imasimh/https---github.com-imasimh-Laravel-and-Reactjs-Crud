bdb9bac661064c35b8f1b11f4aade70dbe8da642
Asim Hussain <asimofficial1998@gmail.com>
2023-02-02 21:16:26 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Routes, Route, Link } from "react-router-dom"; 
+import Home from './pages/home';
+import Create from './pages/create';
+import Edit from './pages/edit';
+import View from './pages/view';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <nav className='navbar navbar-expand navbar-dark bg-primary'>
+        <div className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link to={"/"} className="nav-link">Home</Link>
+          </li>
+          <li className='nav-item'>
+            <Link to={"/create"} className="nav-link">Add New User</Link>
+          </li>
+         </div>
+      </nav>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path='/view/:id' element={<View />} />
+        </Routes>
+      </div>
+      
     </div>
   );
 }
---
+++ src/pages/create.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import http from "../http"
+import { useNavigate } from "react-router-dom";
+
+export default function Create(){
+    const [inputs,setInputs]=useState({});
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values,[name]:value}))
+    }
+    const submitForm = () =>{
+        http.post('/users',inputs).then((res)=>{
+            navigate('/');
+        })
+    }
+    return(
+        <div>
+            <h2>Add New User</h2>
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card p-4">
+                        <label>Name</label>
+                        <input type="text" name="name" className="form-control mb-2"
+                                 value={inputs.name || ''}
+                                 onChange={handleChange}
+                             />
+
+                        <label>Email</label>
+                        <input type="email" name="email" className="form-control mb-2"
+                          value={inputs.email || ''}
+                          onChange={handleChange}
+                        />
+
+                        <label>Password</label>
+                        <input type="password" name="password" className="form-control mb-2"
+                            value={inputs.password || ''}
+                            onChange={handleChange}
+                             />
+
+                        <button type="button" onClick={submitForm} className="btn btn-info mt-2">Create</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
